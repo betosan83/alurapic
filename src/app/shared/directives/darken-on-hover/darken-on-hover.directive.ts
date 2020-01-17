413a74d8e880339530019de96d7b67815b5d59bb
roberto.bfigueredo <roberto.bfigueredo@dataprev.gov.br>
2020-01-17 17:12:29 -0300

    Novo projeto do curso

---
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener, Renderer, Input } from '@angular/core';
+
+@Directive({
+    //Esta diretiva será utilizada como um atributo, por isso os colchetes.
+    selector: '[apDarkenOnHover]'
+})
+export class DarkenOnHoverDirective { 
+
+    @Input() brightness = '70%';
+
+    constructor(private el: ElementRef, private renderer: Renderer) {
+
+    }
+
+    @HostListener('mouseover')
+    darkenOn() {
+        this.renderer.setElementStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+    }
+
+    @HostListener('mouseleave')
+    darkenOff() {
+        this.renderer.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+    }
+
+}
