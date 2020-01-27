ce4f774db55b9a6e9ddfa37f90925a2443d2ff3e
roberto.bfigueredo <roberto.bfigueredo@dataprev.gov.br>
2020-01-27 08:13:21 -0300

    Nova versão do sistema.

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { User } from './user';
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    //O BehaviorSubject precisa ser inicializado com a alguma coisa, diferentemente do subject.
+    //Resolve o problema de se perder o token ao recarregar a página.
+    private userSubject = new BehaviorSubject<User>(null);
+    private userName: string;
+
+    constructor(private tokenService: TokenService) {
+        this.tokenService.hasToken() &&
+            this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userName = user.name;
+        this.userSubject.next(user);    
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    logout() {
+        this.tokenService.removeToken();
+        this.userSubject.next(null);
+    }
+
+    isLogged() {
+        return this.tokenService.hasToken();
+    }
+
+    getUserName() {
+        return this.userName;
+    }
+}
